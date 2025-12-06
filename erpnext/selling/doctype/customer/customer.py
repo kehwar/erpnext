b292abe75b393d8759411a57a096c838b7898dc2
kehwar <erickkwr@gmail.com>
2025-12-06 05:57:44 +0000

    fix: adjust sales team allocation percentage validation

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -156,8 +156,8 @@ class Customer(TransactionBase):
 				self.loyalty_program_tier = customer.loyalty_program_tier
 
 		if self.sales_team:
-			if sum(member.allocated_percentage or 0 for member in self.sales_team) != 100:
-				frappe.throw(_("Total contribution percentage should be equal to 100"))
+			if abs(sum(member.allocated_percentage or 0 for member in self.sales_team) - 100) > 0.00001:
+				self.sales_team[-1].allocated_percentage = round(100 - sum(member.allocated_percentage or 0 for member in self.sales_team[:-1]), 2)
 
 	@frappe.whitelist()
 	def get_customer_group_details(self):
